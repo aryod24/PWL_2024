e157e8b2cc239c3ea1f9d6f5c5d49dff286af83b
aryod24 <aryowahyu24@gmail.com>
2024-09-04 09:09:07 +0700

    JS2 P1 Basic Routing

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,28 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/welcome', function () { 
+    return 'Selamat Datang';
 });
+
+Route::get('/about', function () {
+    return 'Nama : Aryo Wahyu Nugroho <br>NIM : 2241760074';
+ });
+
+Route::get('/user/aryo', function($name = 'Aryo Wahyu Nugroho') {
+    return 'Nama Saya: ' . $name;
+ });
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{post}/comments/{comment}', function ($commentId) {
+    return 'Halaman Artikel ini '."dengan ID ".$commentId;
+});
+
+Route::get('/user/{name?}', function ($name='John') { 
+    return 'Nama saya '.$name;
+});
+
+
