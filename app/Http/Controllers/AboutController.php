15ae6893ad09beb0d6eddc33fb1d660af05294bd
aryod24 <aryowahyu24@gmail.com>
2024-09-04 09:45:35 +0700

    JS2 P2 - Membuat Controller

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about() 
+    {
+        return "Nama : Aryo Wahyu Nugroho
+        <br>
+        NIM : 2241760074";
+        }
+        
+}
+
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,29 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\PhotoController;
+
+Route::resource('photos', PhotoController::class);
+
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+]);
+
+Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+]);
+
+Route::get('/hello', [WelcomeController::class, 'hello']);
+
+Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/about', [AboutController::class, 'about']);
+
+Route::get('/articles/{id}', [ArticleController::class, 'articles']);
 
 Route::get('/welcome', function () { 
     return 'Selamat Datang';
@@ -25,5 +48,7 @@ Route::get('/articles/{post}/comments/{comment}', function ($commentId) {
 Route::get('/user/{name?}', function ($name='John') { 
     return 'Nama saya '.$name;
 });
+
+
 
 
